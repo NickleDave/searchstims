172773aac41e7c71908440b4ebb82a817783aa73
David Nicholson <nicholdav@gmail.com>
2018-05-22 22:37:26 -0400

    fix up utils and script, delete __pycache__

--- script.py
+++ script.py
@@ -1,21 +1,35 @@
+import os
+
 import pygame
 
 from visualsearchstim.utils import make_rectangle_stim
 
 if __name__ == '__main__':
-	num_target_present = 10
-	num_target_absent = 10
-	total_set_size = 8
 
-	num_distractors_target_present = total_set_size - 1
-	for i in range(num_target_present):
-		filename = 'feature_search_stimulus_target_present_{}.png'.format(i)
-		surface = make_rectangle_stim(num_distractor=num_distractors_target_present)
-		pygame.image.save(surface, filename)
+    output_dir = os.path.join('.', 'output')
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
 
-	num_distractors_target_absent = total_set_size
-	for i in range(num_target_absent):
-		filename = 'feature_search_stimulus_target_absent_{}.png'.format(i)
-		surface = make_rectangle_stim(num_target=0,
-									  num_distractor=num_distractors_target_absent)
-		pygame.image.save(surface, filename)
+    num_target_present = 4800
+    num_target_absent = 4800
+    set_sizes = [3, 6, 12, 18]
+
+
+    for set_size in set_sizes:
+        num_distractors = set_size - 1
+        for i in range(num_target_present // len(set_sizes)):
+            filename = ('redvert_v_greenvert_set_size_{}_'
+                        'target_present_{}.png'.format(set_size, i))
+            filename = os.path.join(output_dir, filename)
+            surface = make_rectangle_stim(num_target=1,
+                                          num_distractor=num_distractors)
+            pygame.image.save(surface, filename)
+
+        num_distractors_target_absent = set_size
+        for i in range(num_target_absent // len(set_sizes)):
+            filename = ('redvert_v_greenvert_set_size_{}_'
+                        'target_absent_{}.png'.format(set_size, i))
+            filename = os.path.join(output_dir, filename)
+            surface = make_rectangle_stim(num_target=0,
+                                          num_distractor=num_distractors)
+            pygame.image.save(surface, filename)
