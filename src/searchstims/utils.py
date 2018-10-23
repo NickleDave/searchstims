2fcd8fdcbe7c2400126f8d366e1d58d50fa48368
nickledave <nickledave@users.noreply.github.com>
2018-10-23 00:17:46 -0400

    Add StimMaker class and RectangleStimMaker subclass to utils.py

--- src/searchstims/utils.py
+++ src/searchstims/utils.py
@@ -28,6 +28,139 @@ numbers_dict = {
 }
 
 
+class StimMaker:
+    """parent class for all StimMaker classes"""
+    def __init__(self,
+                 set_size=8,
+                 num_target=1,
+                 target_color='red',
+                 distractor_color='green',
+                 grid_size=(5, 5),
+                 window_size=(227, 227),
+                 rects_width_height=(10, 30),
+                 jitter=5
+                 ):
+        if type(set_size) != int:
+            raise TypeError('set size must be an integer')
+
+        if not set_size > 0:
+            raise ValueError('set size must be greater than zero')
+
+        if type(num_target) != int:
+            raise TypeError('number of targets must be an integer')
+
+        if not num_target >= 0:
+            raise ValueError('number of targets must be greater than or equal to zero')
+
+        if num_target > set_size:
+            raise ValueError('number of targets cannot be greater than set size')
+
+        if not all([type(grid_size_el) == int for grid_size_el in grid_size]):
+            raise ValueError('values for grid size must be positive integers')
+
+        if not all([grid_size_el > 0 for grid_size_el in grid_size]):
+            raise ValueError('values for grid size must be positive integers')
+
+        self.set_size = set_size
+        self.num_target = num_target
+        self.target_color = target_color
+        self.distractor_color = distractor_color
+        self.grid_size = grid_size
+        self.window_size = window_size
+        self.rects_width_height = rects_width_height
+        self.jitter = jitter
+
+    def _return_rect_for_stim(self, DISPLAYSURF, rect_to_draw, color):
+        """this function must return a pygame.rect object
+        which the StimMaker.make_stim method will `blit` on
+        the stimulus."""
+        rect = pygame.draw.rect(DISPLAYSURF, colors_dict[color], rect_to_draw)
+        return rect
+
+    def make_stim(self):
+        """make visual search stimuli with rectangles"""
+
+        total_grid_elements = self.grid_size[0] * self.grid_size[1]
+        if self.set_size > total_grid_elements:
+            raise ValueError('set size {} cannot be greater than number of '
+                             'elements in grid, {}'
+                             .format(self.set_size, total_grid_elements))
+
+        if type(self.jitter) != int:
+            raise TypeError('value for jitter must be an integer')
+
+        # make grid, randomly select which cells in grid to use
+        grid_x = np.arange(1, self.grid_size[0] + 1)
+        grid_y = np.arange(1, self.grid_size[1] + 1)
+        xx, yy = np.meshgrid(grid_x, grid_y)
+        xx = xx.ravel()
+        yy = yy.ravel()
+        num_cells = self.grid_size[0] * self.grid_size[1]
+        cells_to_use = sorted(np.random.choice(np.arange(num_cells),
+                                               size=self.set_size,
+                                               replace=False))
+        xx_to_use = xx[cells_to_use]
+        yy_to_use = yy[cells_to_use]
+
+        # find centers of cells we're going to use
+        cell_width = round(self.window_size[0] / self.grid_size[0])
+        cell_x_center = round((self.window_size[0] / self.grid_size[0]) / 2)
+        xx_to_use_ctr = (xx_to_use * cell_width) - cell_x_center
+        cell_height = round(self.window_size[1] / self.grid_size[1])
+        cell_y_center = round((self.window_size[1] / self.grid_size[1]) / 2)
+        yy_to_use_ctr = (yy_to_use * cell_height) - cell_y_center
+
+        # add jitter to those points
+        jitter_high = self.jitter // 2
+        jitter_low = -jitter_high
+        if jitter % 2 == 0:  # if even
+            # have to account for zero at center of jitter range
+            # (otherwise range would be jitter + 1)
+            # (Not a problem when doing floor division on odd #s)
+            coin_flip = np.random.choice([0, 1])
+            if coin_flip == 0:
+                jitter_low += 1
+            elif coin_flip == 1:
+                jitter_high -= 1
+        jitter_range = np.arange(jitter_low, jitter_high + 1)
+
+        x_jitter = np.random.choice(jitter_range, size=xx_to_use.size)
+        xx_to_use_ctr += x_jitter
+        y_jitter = np.random.choice(jitter_range, size=yy_to_use.size)
+        yy_to_use_ctr += y_jitter
+
+        # set up window
+        DISPLAYSURF = pygame.display.set_mode(self.window_size, 0, 32)
+
+        # draw on surface object
+        DISPLAYSURF.fill(colors_dict['black'])
+        target_inds = np.random.choice(np.arange(self.set_size),
+                                       size=self.num_target).tolist()
+        rects = []
+        for item in range(self.set_size):
+            rect_tuple = (0, 0) + self.rects_width_height
+            rect_to_draw = Rect(rect_tuple)
+            rect_to_draw.center = (xx_to_use_ctr[item], yy_to_use_ctr[item])
+            if item in target_inds:
+                curr_rect = self._return_rect_for_stim(DISPLAYSURF,
+                                                       rect_to_draw,
+                                                       self.target_color)
+            else:
+                curr_rect = self._return_rect_for_stim(DISPLAYSURF,
+                                                       rect_to_draw,
+                                                       self.distractor_color)
+            rects.append(curr_rect)
+
+        pygame.display.update()
+        return DISPLAYSURF
+
+
+class RectangleStimMaker(StimMaker):
+    def _return_rect_for_stim(self, DISPLAYSURF, rect_to_draw, color):
+        rect = pygame.draw.rect(DISPLAYSURF, colors_dict[color], rect_to_draw)
+        return rect
+
+
 def make_rectangle_stim(set_size=8,
                         num_target=1,
                         target_color='red',
@@ -242,6 +375,3 @@ def make_number_stim(set_size=8,
     pygame.display.update()
     return DISPLAYSURF
 
-
-if __name__ == '__main__':
-    make_rectangle_stim()
