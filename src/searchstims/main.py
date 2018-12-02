06749c5e25d1e6b6dfff83c09f4f4a92d19e0fb3
NickleDave <nickledave@users.noreply.github.com>
2018-12-02 11:17:16 -0500

    in main.py, factor out config parsing into separate function parse_config

--- src/searchstims/main.py
+++ src/searchstims/main.py
@@ -3,26 +3,44 @@ import ast
 import configparser
 import json
 import os
+from typing import NamedTuple
 
 import pygame
 
 from . import stim_makers
 
 
-def make(config):
-    """actual function that makes all the stimuli
+class ConfigTuple(NamedTuple):
+    """represent config"""
+    num_target_present: int
+    num_target_absent: int
+    set_sizes: list
+    stimulus: str
+    output_dir: str
+    json_filename: str
+
+
+def parse_config(config_file):
+    """parse config; convert strings to correct types
 
     Parameters
     ----------
-    config : configparser.ConfigParser
-        that already has a config.ini file loaded using the read method
+    config_file : str
+        filename of a config.ini file
 
     Returns
     -------
-    None
-
-    saves all the stimuli to config['config']['output_dir']
+    config_tuple : ConfigTuple
+        with fields
+            num_target_present: int
+            num_target_absent: int
+            set_sizes: list
+            stimulus: str
+            output_dir: str
+            json_filename: str
     """
+    config = configparser.ConfigParser()
+    config.read(config_file)
     num_target_present = int(config['config']['num_target_present'])
     num_target_absent = int(config['config']['num_target_absent'])
     set_sizes = ast.literal_eval(config['config']['set_sizes'])
@@ -45,7 +63,29 @@ def make(config):
         json_filename = os.path.join(output_dir,
                                      'filenames_by_set_size_'
                                      'and_target.json')
+    return ConfigTuple(num_target_present,
+                       num_target_absent,
+                       set_sizes,
+                       stimulus,
+                       output_dir,
+                       json_filename
+                       )
 
+
+def make(config_tuple):
+    """actual function that makes all the stimuli
+
+    Parameters
+    ----------
+    config_tuple : ConfigTuple
+        returned by parse_config after parsing a config.ini file
+
+    Returns
+    -------
+    None
+
+    saves all the stimuli to config_tuple.output_dir
+    """
     # put filenames in a dict that we save as json
     # so we don't have to do a bunch of string matching to find them later,
     # instead we can just get all the filenames for a given set size
@@ -53,39 +93,39 @@ def make(config):
     # by using appropriate keys
     # e.g. fnames_set_size_8_target_present = filenames_dict[8]['present']
     filenames_dict = {}
-    if stimulus == 'rectangle':
+    if config_tuple.stimulus == 'rectangle':
         stim_maker = stim_makers.RectangleStimMaker()
-    elif stimulus == 'number':
+    elif config_tuple.stimulus == 'number':
         stim_maker = stim_makers.NumberStimMaker()
 
-    for set_size in set_sizes:
+    for set_size in config_tuple.set_sizes:
         # add dict for this set size that will have list of "target present / absent" filenames
         filenames_dict[set_size] = {}
         for target in ('present', 'absent'):
             # add the actual filename list for 'present' or 'absent'
             filenames_dict[set_size][target] = []
             if target == 'present':
-                inds_of_stim_to_make = range(num_target_present // len(set_sizes))
+                inds_of_stim_to_make = range(config_tuple.num_target_present // len(config_tuple.set_sizes))
                 num_target = 1
             elif target == 'absent':
-                inds_of_stim_to_make = range(num_target_absent // len(set_sizes))
+                inds_of_stim_to_make = range(config_tuple.num_target_absent // len(config_tuple.set_sizes))
                 num_target = 0
 
             for i in inds_of_stim_to_make:
-                if stimulus == 'rectangle':
+                if config_tuple.stimulus == 'rectangle':
                     filename = ('redvert_v_greenvert_set_size_{}_'
                                 'target_{}_{}.png'.format(set_size, target, i))
-                elif stimulus == 'number':
+                elif config_tuple.stimulus == 'number':
                     filename = ('two_v_five_set_size_{}_'
                                 'target_{}_{}.png'.format(set_size, target, i))
                     surface = stim_maker.make_stim(set_size=set_size,
                                                    num_target=num_target)
-                filename = os.path.join(output_dir, filename)
+                filename = os.path.join(config_tuple.output_dir, filename)
                 pygame.image.save(surface, filename)
                 filenames_dict[set_size][target].append(filename)
 
     filenames_json = json.dumps(filenames_dict, indent=4)
-    with open(json_filename, 'w') as json_output:
+    with open(config_tuple.json_filename, 'w') as json_output:
         print(filenames_json, file=json_output)
 
 
@@ -100,10 +140,8 @@ def main():
     config_file = args.config
     if not os.path.isfile(config_file):
         raise FileNotFoundError("Config file {} not found".format(config_file))
-    # config parsing boilerplate
-    config = configparser.ConfigParser()
-    config.read(args.config)
-    make(config)
+    config_tuple = parse_config(config_file)
+    make(config_tuple)
 
 
 if __name__ == '__main__':
