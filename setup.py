cbb4adc8db0b76ad9fe3ae0e3f493c8ed75b6b82
NickleDave <nickledave@users.noreply.github.com>
2018-12-01 22:28:51 -0500

    switch to Kenneth Reitz setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
+# adopted from Kenneth Reitz
+# https://github.com/kennethreitz/setup.py
+# released under MIT license
+# (https://github.com/kennethreitz/setup.py/blob/master/LICENSE)
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = 'searchstims'
@@ -8,7 +22,7 @@ DESCRIPTION = 'generate images like the stimuli used in visual search experiment
 URL = 'https://github.com/NickleDave/main.py'
 EMAIL = 'nicholdav@gmail.com'
 AUTHOR = 'David Nicholson'
-VERSION = '0.1'
+VERSION = '0.1.1a1'
 KEYWORDS = ['visual search', 'pygame']
 
 # What packages are required for this module to be executed?
@@ -23,36 +37,107 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# this is here so that the .travis.yml script can install
+# dependencies just for the tests by running
+# pip install .[tests]
+EXTRAS = {
+    # None
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
     name=NAME,
-    version=VERSION,
+    version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
     url=URL,
-    keywords=KEYWORDS,
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where="src", exclude=('tests',)),
+    package_dir={"": "src"},
     entry_points={
         'console_scripts': ['searchstims=searchstims.main:main'],
     },
     install_requires=REQUIRED,
-    python_requires='>=3',
+    extras_require=EXTRAS,
     include_package_data=True,
-    license='BSD-3',
+    license=LICENSE,
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: BSD License',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
