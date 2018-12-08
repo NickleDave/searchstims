c28cf33bbeecad532f0b0cc530a3697c21642179
NickleDave <nickledave@users.noreply.github.com>
2018-12-08 16:59:48 -0500

    rename test_main.py to test_make.py since that's what it's actually doing

--- tests/test_make.py
+++ tests/test_make.py
@@ -1,5 +1,5 @@
 """
-test main module
+test make module
 """
 import os
 import tempfile
@@ -7,7 +7,7 @@ import shutil
 from configparser import ConfigParser
 import unittest
 
-import searchstims.main
+import searchstims.make
 
 
 HERE = os.path.dirname(__file__)
@@ -24,25 +24,25 @@ class TestMain(unittest.TestCase):
 
     def test_make_number(self):
         config = ConfigParser()
-        config.add_section('config')
-        config['config']['num_target_present'] = 25
-        config['num_target_absent'] = 25
-        config['set_sizes'] = [1, 2, 4, 6, 8]
+        config.add_section('general')
+        config['config']['num_target_present'] = '25'
+        config['num_target_absent'] = '25'
+        config['set_sizes'] = ''[1, 2, 4, 6, 8]''
         config['output_dir'] = self.tmp_output_dir
         config['json_filename'] = 'filenames_by_set_size_and_target.json'
         config['stimulus'] = 'number'
-        searchstims.main.make(config)
+        searchstims.make(config)
 
     def test_make_rectangle(self):
         config = ConfigParser()
-        config.add_section('config')
-        config['config']['num_target_present'] = 25
-        config['num_target_absent'] = 25
-        config['set_sizes'] = [1, 2, 4, 6, 8]
+        config.add_section('general')
+        config['config']['num_target_present'] = '25'
+        config['num_target_absent'] = '25'
+        config['set_sizes'] = '[1, 2, 4, 6, 8]'
         config['output_dir'] = self.tmp_output_dir
         config['json_filename'] = 'filenames_by_set_size_and_target.json'
         config['stimulus'] = 'rectangle'
-        searchstims.main.make(config)
+        searchstims.make(config)
 
 
 if __name__ == '__main__':
