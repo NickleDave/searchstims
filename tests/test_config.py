2dacf9a2ec9b44462773a08a844dcb835072ae1a
NickleDave <nickledave@users.noreply.github.com>
2018-12-08 17:23:15 -0500

    flesh out config tests, basic_config passes

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,7 +2,6 @@
 test config module
 """
 import os
-import tempfile
 import shutil
 from configparser import ConfigParser
 import unittest
@@ -15,11 +14,32 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 class TestConfig(unittest.TestCase):
     def setUp(self):
-        self.test_configs = os.path.join(HERE, 'test_data', 'config')
-        self.tmp_output_dir = tempfile.mkdtemp()
+        self.test_configs = os.path.join(HERE, 'test_data', 'configs')
 
-    def tearDown(self):
-        shutil.rmtree(self.tmp_output_dir)
+    def tuple_fields_equal_ConfigParser_sections(self, config_tuple, config_parser_obj):
+        if list(config_tuple._fields) == config_parser_obj.sections():
+            return True
+        else:
+            return False
+
+    def tuple_section_fields_equal_ConfigParser_options(self, config_tuple, config_parser_obj):
+        tuple_sections = sorted(list(config_tuple._fields))
+        parser_sections = sorted(config_parser_obj.sections())
+        tuple_dict = config_tuple._asdict()
+        fields_equal_to_options = []
+        for tup_sect, parse_sect in zip(tuple_sections, parser_sections):
+            section_tuple = tuple_dict[tup_sect]
+            section_tuple_fields = sorted(list(section_tuple._fields))
+            section_options = sorted(config_parser_obj.options(parse_sect))
+            if section_tuple_fields == section_options:
+                fields_equal_to_options.append(True)
+            else:
+                fields_equal_to_options.append(False)
+
+        if all(fields_equal_to_options):
+            return True
+        else:
+            return False
 
     def test_parse_basic_config(self):
         # get file we need and load into ConfigParser instance to use for tests
@@ -30,9 +50,11 @@ class TestConfig(unittest.TestCase):
         # now run through parser
         basic_config_tup = searchstims.config.parse(basic_config_file)
 
-        sections = [key for key in list(config.keys()) if key != 'DEFAULT']
-        self.assertTrue(basic_config_tup._fields == sections)
+        self.assertTrue(self.tuple_fields_equal_ConfigParser_sections(basic_config_tup,
+                                                                      basic_config))
+        self.assertTrue(self.tuple_section_fields_equal_ConfigParser_options(basic_config_tup,
+                                                                             basic_config))
 
 
-if __name == '__main__':
+if __name__ == '__main__':
     unittest.main()
