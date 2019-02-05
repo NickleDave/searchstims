cabe1505fe0b0d1587bcb06523c15af0edde4578
nickledave <nickledave@users.noreply.github.com>
2019-02-04 20:44:01 -0500

    make configfile mandatory arg in main, not optional

--- src/searchstims/main.py
+++ src/searchstims/main.py
@@ -7,13 +7,14 @@ from .make import make
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config',
+    parser.add_argument('configfile',
                         type=str,
-                        help=('filename of config file. '
+                        help=('filename of config.ini file, e.g.:\n'
+                              '$ searchstims ./basic_config.ini\n'
                               'For an example config.ini file, see: '
                               'https://github.com/NickleDave/searchstims'))
     args = parser.parse_args()
-    config_file = args.config
+    config_file = args.configfile
     if not os.path.isfile(config_file):
         raise FileNotFoundError("Config file {} not found".format(config_file))
     config_tuple = parse(config_file)
