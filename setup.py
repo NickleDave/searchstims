1e5522df4f769fb8f0227e0b6838bdca49586c63
David Nicholson <nicholdav@gmail.com>
2019-02-22 15:45:56 -0500

    bump pre-release version in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = 'https://github.com/NickleDave/main.py'
 EMAIL = 'nicholdav@gmail.com'
 AUTHOR = 'David Nicholson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.0a4'
+VERSION = '0.2.0a5'
 LICENSE = 'BSD'
 
 # What packages are required for this module to be executed?
