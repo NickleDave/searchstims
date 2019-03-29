6f055f71391cb489feba0f686cd3bf117328b58c
NickleDave <nicholdav@gmail.com>
2019-03-29 19:18:36 -0400

    TestConfig tests whether options not set by user were set to default by searchstims.config.parse

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,6 +18,33 @@ class TestConfig(unittest.TestCase):
         self.test_configs = os.path.join(HERE, 'test_data', 'configs')
         self.default_config = ConfigParser()
         self.default_config.read(DEFAULT_CONFIG_FILE)
+        self.default_config_obj = searchstims.config.parse(DEFAULT_CONFIG_FILE)
+
+    def _test_defaults_set_correctly(self,
+                                     config_parser,
+                                     config_obj,
+                                     section_name_list):
+        """helper function that checks all option not set by user are
+        set to default by searchstims.config.parse"""
+        option_was_set_to_default = []
+        for section in section_name_list:
+            section_obj = getattr(config_obj, section)
+            default_section_obj = getattr(self.default_config_obj, section)
+            default_section = self.default_config[section]
+            config_section = config_parser[section]
+            for option in default_section.keys():
+                if option not in config_section:
+                    # if user did not set this option
+                    option_was_set_to_default.append(
+                        # make sure it equals default
+                        getattr(section_obj, option) == getattr(default_section_obj, option)
+                    )
+
+        if all(option_was_set_to_default):
+            return True
+        else:
+            return False
+
 
     def test_no_general_section_raises_error(self):
         tmp_dir = tempfile.mkdtemp()
@@ -40,6 +67,9 @@ class TestConfig(unittest.TestCase):
         self.assertTrue(hasattr(rectangle_config_obj, 'general'))
         self.assertTrue(hasattr(rectangle_config_obj, 'rectangle'))
         self.assertTrue(rectangle_config_obj.number is None)
+        self.assertTrue(self._test_defaults_set_correctly(
+            rectangle_config, rectangle_config_obj, ['general', 'rectangle']
+        ))
 
     def test_parse_number_config(self):
         # get file we need and load into ConfigParser instance to use for tests
@@ -52,6 +82,10 @@ class TestConfig(unittest.TestCase):
         self.assertTrue(hasattr(number_config_obj, 'number'))
         self.assertTrue(number_config_obj.rectangle is None)
 
+        self.assertTrue(self._test_defaults_set_correctly(
+            number_config, number_config_obj, ['general', 'number']
+        ))
+
     def test_parse_rectangle_number_config(self):
         # get file we need and load into ConfigParser instance to use for tests
         config_file = os.path.join(self.test_configs, 'config_feature_spatial_vgg16.ini')
@@ -62,6 +96,10 @@ class TestConfig(unittest.TestCase):
         self.assertTrue(hasattr(config_obj, 'general'))
         self.assertTrue(hasattr(config_obj, 'rectangle'))
         self.assertTrue(hasattr(config_obj, 'number'))
+
+        self.assertTrue(self._test_defaults_set_correctly(
+            config, config_obj, ['general', 'rectangle', 'number']
+        ))
 
 
 if __name__ == '__main__':
