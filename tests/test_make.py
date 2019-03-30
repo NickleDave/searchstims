99ee16e17adcada1323778167aa8768d59857b17
NickleDave <nicholdav@gmail.com>
2019-03-29 21:01:36 -0400

    rewrite tests/test_make.py to use config.ini files in tests/test_data/config

--- tests/test_make.py
+++ tests/test_make.py
@@ -12,42 +12,36 @@ from searchstims.make import make
 
 
 HERE = os.path.dirname(__file__)
-TEST_CONFIGS = os.path.join(HERE, 'test_data', 'config')
 
 
 class TestMain(unittest.TestCase):
 
     def setUp(self):
+        self.test_configs = os.path.join(HERE, 'test_data', 'configs')
         self.tmp_output_dir = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(self.tmp_output_dir)
 
-    def test_make_number(self):
-        config = ConfigParser()
-        config.add_section('general')
-        config['general']['num_target_present'] = '25'
-        config['general']['num_target_absent'] = '25'
-        config['general']['set_sizes'] = '[1, 2, 4, 6, 8]'
-        config['general']['output_dir'] = self.tmp_output_dir
-        config['general']['json_filename'] = 'filenames_by_set_size_and_target.json'
-        config.add_section('number')
-        config['number']['rects_width_height'] = '(30,30)'
-        config_tup = parse(config=config)
-        make(config_tup)
-
     def test_make_rectangle(self):
-        config = ConfigParser()
-        config.add_section('general')
-        config['general']['num_target_present'] = '25'
-        config['general']['num_target_absent'] = '25'
-        config['general']['set_sizes'] = '[1, 2, 4, 6, 8]'
-        config['general']['output_dir'] = self.tmp_output_dir
-        config['general']['json_filename'] = 'filenames_by_set_size_and_target.json'
-        config.add_section('rectangle')
-        config['rectangle']['rects_width_height'] = '(10,30)'
-        config_tup = parse(config=config)
-        make(config_tup)
+        rectangle_config_file = os.path.join(self.test_configs, 'test_rectangle_config.ini')
+        config_obj = parse(rectangle_config_file)
+        config_obj.general.output_dir = self.tmp_output_dir
+        make(config_obj)
+
+    def test_make_number(self):
+        number_config_file = os.path.join(self.test_configs, 'test_number_config.ini')
+        config_obj = parse(number_config_file)
+        config_obj.general.output_dir = self.tmp_output_dir
+        make(config_obj)
+
+    def test_make_number_and_rectangle(self):
+        number_config_file = os.path.join(
+            self.test_configs, 'test_config_feature_spatial_vgg16.ini'
+        )
+        config_obj = parse(number_config_file)
+        config_obj.general.output_dir = self.tmp_output_dir
+        make(config_obj)
 
 
 if __name__ == '__main__':
