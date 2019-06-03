722e1874eac2ee72db9df9a028b1118c48109e87
NickleDave <nickledave@users.noreply.github.com>
2019-06-03 13:37:41 -0400

    stim_makers/__init__.py imports TStimMaker

--- src/searchstims/stim_makers/__init__.py
+++ src/searchstims/stim_makers/__init__.py
@@ -2,3 +2,4 @@ from .abstract_stim_maker import AbstractStimMaker
 from .RVvGVStimMaker import RVvGVStimMaker
 from .RVvRHGVStimMaker import RVvRHGVStimMaker
 from .Two_v_Five_StimMaker import Two_v_Five_StimMaker
+from .TStimMaker import TStimMaker
