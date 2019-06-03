b0d1a10d73bd47306c6df3f5950d0e3960c38099
NickleDave <nickledave@users.noreply.github.com>
2019-06-03 10:25:23 -0400

    stim_makers/__init__.py imports StimMaker classes from their modules

---
+++ src/searchstims/stim_makers/__init__.py
@@ -0,0 +1,4 @@
+from .abstract_stim_maker import AbstractStimMaker
+from .RVvGVStimMaker import RVvGVStimMaker
+from .RVvRHGVStimMaker import RVvRHGVStimMaker
+from .Two_v_Five_StimMaker import Two_v_Five_StimMaker
