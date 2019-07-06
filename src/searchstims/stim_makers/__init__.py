386871c4a95cc1ae5fb0063dda4d2e43aa3062c8
NickleDave <nickledave@users.noreply.github.com>
2019-07-06 19:33:26 -0400

    searchstims/stim_makers/__init__.py imports xoStimMaker

--- src/searchstims/stim_makers/__init__.py
+++ src/searchstims/stim_makers/__init__.py
@@ -4,3 +4,4 @@ from .RVvRHGVStimMaker import RVvRHGVStimMaker
 from .Two_v_Five_StimMaker import Two_v_Five_StimMaker
 from .TStimMaker import TStimMaker
 from .TLStimMaker import TLStimMaker
+from .xoStimMaker import xoStimMaker
